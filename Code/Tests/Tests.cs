81abf1c1f3417b0d4c89d8379c7f0ea310d92996
Jean Miguel Dotres Lopez <jeandotres23@gmail.com>
2025-08-20 14:02:36 +0200

    Function Time

--- Code/Tests/Tests.cs
+++ Code/Tests/Tests.cs
@@ -23,8 +23,47 @@ public static class DataRepoTests
 
     public static void Test1()
     {
+        var repo = CreateTestRepoEntries(100000);
+    }
+
+    public static DataRepo CreateTestRepoEntries(int entries)
+    {
+        var repo = new DataRepo();
+
+        
+        var swTotal = Stopwatch.StartNew();
+        for (int i = 0; i < entries; i++)
+        {
+            string key = $"Test.Key.{i}";
+            var type = RepoValueType.String;
+            string value = $"Value {i}";
+
+            repo.CreateAndWriteKey(key, type, value);
+
+        }
+
+        swTotal.Stop();
+        Console.WriteLine($"Total Time CreateAndWriteKey * {entries}: {swTotal.ElapsedMilliseconds} ms");
 
 
+        swTotal.Restart();
+        for (int i = 0; i < entries; i++)
+        {
+            repo.WriteKey($"Test.Key.{i}", $"Updated {i}");
+        }
+        swTotal.Stop();
+        Console.WriteLine($"Total Time WriteKey * {entries}: {swTotal.ElapsedMilliseconds} ms");
+
+        swTotal.Restart();
+        for (int i = 0; i < entries; i++)
+        {
+            var value = repo.ReadKey($"Test.Key.{i}");
+        }
+        swTotal.Stop();
+        Console.WriteLine($"Total Time ReadKey * {entries}: {swTotal.ElapsedMilliseconds} ms");
+
+
+        return repo;
     }
 
 }
